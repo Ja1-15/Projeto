64176c632d56a439f0552c7e3a3beeaa1dc405c7
Joao <joaogabrielr806@gmail.com>
2024-07-15 19:41:57 -0300

    first

--- api_teste/android/app/src/main/kotlin/com/example/api_teste/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.api_teste
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- listconfig/android/app/src/main/kotlin/com/example/listconfig/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.listconfig
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
